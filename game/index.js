a2687871e12911310e61adbc41b12565192bb424
angel-chen <rikugou@gmail.com>
2018-05-16 14:38:20 -0400

    Completed xoxo on the honor system

--- game/index.js
+++ game/index.js
@@ -1,6 +1,43 @@
 import {Map} from 'immutable'
+import { createStore } from "redux"
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map()
+
+const MOVE = 'MOVE'
+
+export const move = (turn, position) => {
+  // console.log('this is the turn/position: ', turn, position)
+  return {
+    type: MOVE,
+    position: position,
+    turn: turn
+  }
 }
+
+export const winner = (board) => {
+  
+}
+
+export default function reducer(state = { board, turn: 'X' }, action) {
+  console.log('here', state.turn)
+  switch (action.type) {
+    case MOVE:
+    return {
+      board: state.board.setIn(action.position, state.turn),
+      turn: state.turn === 'X' ? state.turn = 'O' : state.turn = 'X'
+    }
+    default:
+    return state
+  }
+}
+
+// const gameReducer = (state = {board, turn: player}, action) => {
+//   switch(action.type) {
+//     case 'MOVE'
+//       return { }
+//     default:
+//       return state
+//   }
+// }
+
+
